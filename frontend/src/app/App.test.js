2c4101522500e71184d8eb3dd91d21a705c292d9
Michael Tran <michaelhuytran@gmail.com>
2018-11-03 22:30:40 -0700

    [Patch] - Merge dependency fix

--- frontend/src/app/App.test.js
+++ frontend/src/app/App.test.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {connect} from "react-redux";
+jest.mock('./App');
+
+import {fetchAllPosts} from "../posts/PostAction";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(connect(<App />), div);
+});
+
+describe('toggleDrawer', () => {
+  beforeEach(() => {
+    App.mockClear();
+		App.toggleDrawer(true);
+  });
+
+  it('fetchAllPosts', () => {
+    expect(fetchAllPosts).toHaveBeenCalled();
+  });
+
+  it
 });
