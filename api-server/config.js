5d7a0def60b6d38455086a1c188cf6101c4adc73
Michael Tran <michael.tran@walmartlabs.com>
2018-12-03 08:53:38 -0800

    [Revert]

--- api-server/config.js
+++ api-server/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-	keyFilename: './key.json',
 	origin: process.env.ORIGIN || `http://localhost:${exports.port}`,
 	port: process.env.PORT || 3001,
 	projectId: process.env.PROJECT_ID,
